59ca9131a537d819525d67767ff61e68cfa69c41
HumphreyJ <joshhumphrey1@gmail.com>
2020-01-14 21:10:16 -0600

    Have begun writing tests.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
-// Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard, { closed, locked } from '../dashboard/Dashboard';
+import Display from '../display/Display';
+import Controls from '../controls/Controls';
+
+
+it('Renders Dashboard', () => {
+    const container = render(<Dashboard/>)
+    container.findByText(/closed/i)
+    container.findByText(/unlocked/i)
+    render(<Display/>)
+    render(<Controls/>)
+})
+
+
+
+
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,25 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent, act } from '@testing-library/react';
+import Display from './Display';
+import Controls from '../controls/Controls';
+
+it('displays if open/unlocked', () => {
+    const container = render(<Display/>)
+    const unlocked = container.getByText(/unlocked/i)
+    const open = container.getByText(/open/i)
+    expect(unlocked.textContent).toEqual('Unlocked')
+    expect(open.textContent).toEqual('Open')
+})
+
+it('closes gate', () => {
+    const container = render(<Display />)
+    const wrapper = render(<Controls />)
+    const open = container.getByText('Open');
+    const close = wrapper.getByText('Close Gate');
+    act(() => {
+        fireEvent.click(close);
+    })
+
+    expect(open.textContent).toEqual('Closed');
+
+})
